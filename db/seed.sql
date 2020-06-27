8863a51b7b32e463360f51f5249ca1662354e81b
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-27 08:04:39 -0700

    added seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)    
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (id)  
+);
