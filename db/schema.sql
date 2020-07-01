6f28289a92c345cffb090db24558ed76ebf70c97
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-30 22:36:04 -0700

    trying to fix databases- edited seed.sql and added schema

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Tanner", "Nielsen", 1), ("Ali", "Nielsen", 2), ("Becca", "Nielsen", 3), ("Bailey", "Nielsen", 4), ("Gunner", "Nielsen", 5), ("Hunter", "Nielsen", 6), ("Thor", "Nielsen", 7), ("Jenise", "Nielsen", 8);
+
+INSERT INTO department (name)
+VALUES ("Sales Lead"), ("Sales Person"), ("Lead Engineer"), ("Accountant"), ("Software Engineer"), ("Legal Team Lead"), ("Lawyer"), ("Lead Engineer");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales", 75000, 1), ("Sales", 75000, 2), ("Engineering", 125000, 3), ("Finance", 100000, 4), ("Engineering", 75000, 5), ("Legal", 70000, 6), ("Legal", 85000, 7), ("Engineering", 50000, 8);
